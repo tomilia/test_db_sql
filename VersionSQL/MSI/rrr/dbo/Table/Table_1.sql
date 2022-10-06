43d0474042399178327e2550bc30e94445f297b7
tommy.lee <tommylee@nec.com>
2022-10-06 14:23:39 +0800

    - init

---
+++ VersionSQL/MSI/rrr/dbo/Table/Table_1.sql
@@ -0,0 +1,7 @@
+﻿/****** Object:  Table [dbo].[Table_1]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[Table_1](
+	[ggg] [nchar](10) NULL
+) ON [PRIMARY]
